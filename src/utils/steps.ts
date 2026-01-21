0d40edce241e53666a0e403df4ed6de2d72cfda6
CR20 <carlosrojasbenavides23@gmail.com>
2026-01-20 21:23:26 -0400

    Proyecto

--- src/utils/steps.ts
+++ src/utils/steps.ts
@@ -10,18 +10,50 @@ export const STEPS: StepI[] = [
     words: [
       {
         id: 'word-1',
-        completeWord: 'Welcome',
-        incompleteWord: 'Wel_ome',
-        sugestions: ['c', 'd', 'l'],
+        completeWord: 'ESPEJO',
+        incompleteWord: 'Es_ejo',
+        sugestions: ['b', 'p', 'q'],
         completed: false,
-        imageUrl: 'https://example.com/welcome-image.jpg'
+        imageUrl: 'https://share.google/images/LygSYSwT7x4IfBWrd'
       },
       {
         id: 'word-2',
-        completeWord: 'Application',
-        incompleteWord: 'App_ic_tion',
-        sugestions: ['l', 'r', 's'],
+        completeWord: 'MEDUSA',
+        incompleteWord: 'Me_usa',
+        sugestions: ['q', 'd', 'p'],
+        completed: false,
+        imageUrl: 'https'
+      },
+      {
+        id: 'word-3',
+        completeWord: 'TOSTADA',
+        incompleteWord: 'Tosta_a',
+        sugestions: ['p', 'q', 'd'],
+        completed: false,
+        imageUrl: 'https'
+      },
+      {
+        id: 'word-4',
+        completeWord: 'ABEJA',
+        incompleteWord: 'A_eja',
+        sugestions: ['d', 'q', 'b'],
+        completed: false,
+        imageUrl: 'https'
+      }, 
+      {
+        id: 'word-5',
+        completeWord: 'BOSQUE',
+        incompleteWord: 'Bos_ue',
+        sugestions: ['p', 'b', 'q'],
         completed: false
+      },
+      {
+        id: 'word-6',
+        completeWord: 'ARDILLA',
+        incompleteWord: 'Ar_illa',
+        sugestions: ['q', 'p', 'd'],
+        completed: false,
+        imageUrl: 'https'
       }
     ]
   },
@@ -33,19 +65,225 @@ export const STEPS: StepI[] = [
     completed: false,
     words: [
       {
-        id: 'word-3',
-        completeWord: 'Features',
-        incompleteWord: 'Fe_tures',
-        sugestions: ['a', 'e', 'i'],
-        completed: false
+        id: 'word-7',
+        completeWord: 'MAQUINA',
+        incompleteWord: 'Ma_uina',
+        sugestions: ['b', 'q', 'p'],
+        completed: false,
+        imageUrl: 'https'
       },
       {
-        id: 'word-4',
-        completeWord: 'Functionality',
-        incompleteWord: 'Functi_nality',
-        sugestions: ['o', 'u', 'a'],
-        completed: false
-      }
+        id: 'word-8',
+        completeWord: 'MAPA',
+        incompleteWord: 'Ma_a',
+        sugestions: ['p', 'b', 'q'],
+        completed: false,
+        imageUrl: 'https'
+      },
+      {
+        id: 'word-9',
+        completeWord: 'COLIBRI',
+        incompleteWord: 'Coli_ri',
+        sugestions: ['b', 'q', 'p'],
+        completed: false,
+        imageUrl: 'https'
+      },
+      {
+        id: 'word-10',
+        completeWord: 'DISCO',
+        incompleteWord: '_isco',
+        sugestions: ['q', 'b', 'd'],
+        completed: false,
+        imageUrl: 'https'
+      },
+      {
+        id: 'word-11',
+        completeWord: 'LOBO',
+        incompleteWord: 'Lo_o',
+        sugestions: ['p', 'b', 'd'],
+        completed: false,
+        imageUrl: 'https'
+      },
+      {
+        id: 'word-12',
+        completeWord: 'LAPIZ',
+        incompleteWord: 'La_iz',
+        sugestions: ['q', 'd', 'p'],
+        completed: false,
+        imageUrl: 'https'
+      },
+    ]
+  },
+  {
+    id: 'step-3',
+    page: 3,
+    title: 'Features',
+    description: 'Discover the features we offer.',
+    completed: false,
+    words: [
+      {
+        id: 'word-13',
+        completeWord: 'DUENDE',
+        incompleteWord: 'Duen_e',
+        sugestions: ['d', 'q', 'p'],
+        completed: false,
+        imageUrl: 'https'
+      },
+      {
+        id: 'word-14',
+        completeWord: 'TRONPETA',
+        incompleteWord: 'Trom_eta',
+        sugestions: ['p', 'b', 'q'],
+        completed: false,
+        imageUrl: 'https'
+      },
+      {
+        id: 'word-15',
+        completeWord: 'PAQUETE',
+        incompleteWord: 'Pa_uete',
+        sugestions: ['b', 'q', 'p'],
+        completed: false,
+        imageUrl: 'https'
+      },
+      {
+        id: 'word-16',
+        completeWord: 'PIPA',
+        incompleteWord: '_ipa',
+        sugestions: ['q', 'b', 'p'],
+        completed: false,
+        imageUrl: 'https'
+      },
+      {
+        id: 'word-17',
+        completeWord: 'CHEQUE',
+        incompleteWord: 'Che_ue',
+        sugestions: ['q', 'b', 'd'],
+        completed: false,
+        imageUrl: 'https'
+      },
+      {
+        id: 'word-18',
+        completeWord: 'BUFANDA',
+        incompleteWord: 'Bufan_da',
+        sugestions: ['q', 'd', 'p'],
+        completed: false,
+        imageUrl: 'https'
+      },
+    ]
+  },
+  {
+     id: 'step-4',
+     page: 4,
+     title: 'Features',
+     description: 'Discover the features we offer.',
+     completed: false,
+     words: [
+      {
+        id: 'word-19',
+        completeWord: 'PULPO',
+        incompleteWord: 'Pul_o',
+        sugestions: ['d', 'q', 'p'],
+        completed: false,
+        imageUrl: 'https'
+      },
+      {
+        id: 'word-20',
+        completeWord: 'BALLENA',
+        incompleteWord: '_allena',
+        sugestions: ['p', 'b', 'q'],
+        completed: false,
+        imageUrl: 'https'
+      },
+      {
+        id: 'word-21',
+        completeWord: 'PERRO',
+        incompleteWord: '_erro',
+        sugestions: ['b', 'q', 'p'],
+        completed: false,
+        imageUrl: 'https'
+      },
+      {
+        id: 'word-22',
+        completeWord: 'PEZ',
+        incompleteWord: '_ez',
+        sugestions: ['q', 'b', 'p'],
+        completed: false,
+        imageUrl: 'https'
+      },
+      {
+        id: 'word-23',
+        completeWord: 'GLOBO',
+        incompleteWord: 'Glo_o',
+        sugestions: ['q', 'b', 'd'],
+        completed: false,
+        imageUrl: 'https'
+      },
+      {
+        id: 'word-24',
+        completeWord: 'BURBUJA',
+        incompleteWord: 'Bur_uja',
+        sugestions: ['q', 'b', 'p'],
+        completed: false,
+        imageUrl: 'https'
+      },
+    ]
+  },
+  {
+    id: 'step-5',
+    page: 5,
+    title: 'Features',
+    description: 'Discover the features we offer.',
+    completed: false,
+    words: [
+      {
+        id: 'word-25',
+        completeWord: 'ESTANQUE',
+        incompleteWord: 'Estan_ue',
+        sugestions: ['d', 'q', 'p'],
+        completed: false,
+        imageUrl: 'https'
+      },
+      {
+        id: 'word-26',
+        completeWord: 'LIBELULA',
+        incompleteWord: 'Li_elula',
+        sugestions: ['p', 'b', 'q'],
+        completed: false,
+        imageUrl: 'https'
+      },
+      {
+        id: 'word-27',
+        completeWord: 'COMPUTADORA',
+        incompleteWord: 'Com_utadora',
+        sugestions: ['b', 'q', 'p'],
+        completed: false,
+        imageUrl: 'https'
+      },
+      {
+        id: 'word-28',
+        completeWord: 'DIAMANTE',
+        incompleteWord: '_iamante',
+        sugestions: ['q', 'b', 'd'],
+        completed: false,
+        imageUrl: 'https'
+      },
+      {
+        id: 'word-29',
+        completeWord: 'DADO',
+        incompleteWord: 'Da_o',
+        sugestions: ['q', 'b', 'd'],
+        completed: false,
+        imageUrl: 'https'
+      },
+      {
+        id: 'word-30',
+        completeWord: 'PIANO',
+        incompleteWord: '_iano',
+        sugestions: ['q', 'd', 'p'],
+        completed: false,
+        imageUrl: 'https'
+      },
     ]
   }
+
 ];
